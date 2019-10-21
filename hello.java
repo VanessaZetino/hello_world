cedf411cb966b52cabe0a482692b06cf7150ae1a
Vanessa <44982786+VanessaZetino@users.noreply.github.com>
2019-10-21 18:55:39 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,19 +1,14 @@
 // a simple program to practice common Github commands 
 import java.util.Scanner;
-
-
 public class hello {
 
     public static void main(String [] args) {
-
         Scanner input = new Scanner(System.in);
-	
 	System.out.print("Enter your first name: ");
     	String firstName = input.next();
 	System.out.print("Enter your last name: ");
 	String lastName = input.next();
-	
-        System.out.println("Hello, my name is" + firstName + lastName);
+        System.out.println("Hello, my name is" + firstName + " " + lastName);
     }
 }
 
